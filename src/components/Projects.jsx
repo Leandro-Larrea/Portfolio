c1e35b2876ffc04a898f2f2141b3cefa8023708c
unknown <meliodas_17@hotmail.com>
2022-12-20 08:08:27 -0300

    project screens

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,10 +1,15 @@
 import videogames from "../media/images/videogames.png";
 import pokemons from "../media/images/pokemons.png";
 import eddie from "../media/images/eddie.png";
+import eddieMobile from "../media/images/eddieMobile.png";
 import { AnimationOnScroll } from 'react-animation-on-scroll';
 import github from "../media/images/programing-icos/github.png"
 import { useContext } from "react";
 import { ThemeContext } from "../context/ThemeContext";
+import pcScreen from "../media/images/pcScreen.png";
+import phoneScreenCut from "../media/images/phoneScreenCut.png"
+import videoGamesMobile from "../media/images/videoGamesMobile.png"
+
 
 
 export const Projects = ()=>{
@@ -14,14 +19,17 @@ export const Projects = ()=>{
         {link:"https://github.com/Leandro-Larrea/videoGames",
         demo: "https://leangames.vercel.app/",
         description:"Un proyecto individual usando la api de videogames RAWG en el que integre las tecnologias que aprendi en el curso de HENRY, muestra diferentes juegos de la api, los cuales podemos filtrar y ordenar de diferentes formas, ademas permite al usuario postear editar y eliminar su propio videojuego",
-        img:videogames},
+        img:videogames,
+    imgR:videoGamesMobile},
         {link:"https://github.com/Leandro-Larrea/Pokemon",
         demo: "https://pokemon-eight-plum.vercel.app/",
         description:"Un proyecto hecho con la PokeApi, permite al usuario buscar entre los diferentes pokemones asi como crear un pokemon personalizado",
-        img:pokemons},
+        img:pokemons,
+    imgR:pokemons},
          {link:"https://github.com/Leandro-Larrea/Edie-home-page",
          description:"Eddie homepage es un desafio de devchallenges.io el objetivo era crear una pagina identica a la presentada en una imagen, tanto en su version desktop como mobile.",
         img:eddie,
+        imgR:eddieMobile,
         demo:"https://airy-luck-feta.glitch.me/#"}
         ]
 
@@ -39,17 +47,24 @@ export const Projects = ()=>{
                 {a?.map(a=>
                      <AnimationOnScroll duration={.5} animateIn="animate__zoomIn">
                          <div className={`projects-card ${theme}`} key={a.link} >
-                    <div className="projects-img-container" >
-                    <img className="projects-img" src={a.img}></img>
-                    </div>
-                    <div className="projects-description">
-                        <p>{a.description}</p>
-                    </div>
-                    <div className="buttons-container" key={a.link} >
-                        <a key={a.link} className={`button1 ${theme}`}  href={a.demo?a.demo:a.link } target="_blank">Project</a>
-                        <a className={`button2 ${theme}`} href={a.link} target="_blank"><img src={github}/>Code</a>
-                    </div>
-                </div>
+                            <div className="projects-img-container" >
+                                <img className="projects-desktop-screen" src={pcScreen}></img>
+                                <img className="projects-img" src={a.img}></img>
+                                <div className="projects-mobile">
+                                    <div className="projects-mobile-position">
+                                        <img className="projects-mobile-screen" src={phoneScreenCut}></img>
+                                        <img className="projects-mobile-img" src={a.imgR}></img>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="projects-description">
+                                <p>{a.description}</p>
+                            </div>
+                            <div className="buttons-container" key={a.link} >
+                                <a key={a.link} className={`button1 ${theme}`}  href={a.demo?a.demo:a.link } target="_blank">Project</a>
+                                <a className={`button2 ${theme}`} href={a.link} target="_blank"><img src={github}/>{"<Code/>"}</a>
+                            </div>
+                        </div>
                 </AnimationOnScroll>
                 )}
             </div>
