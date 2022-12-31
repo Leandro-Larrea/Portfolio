279a3bba3f5d5c7a1e3fdb220117976bdbfe8814
unknown <meliodas_17@hotmail.com>
2022-12-31 16:39:20 -0300

    troll button working

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,37 +2,67 @@ import React, { useContext, useRef } from 'react';
 import emailjs from '@emailjs/browser';
 import { ThemeContext } from '../context/ThemeContext';
 import letter from "../media/images/letter.png"
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 const { VITE_PUBLIC_KEY, VITE_TEMPLATE_ID, VITE_SERVICE_ID } = import.meta.env
 
 export const ContactUs = () => {
     const { theme, setTheme } = useContext(ThemeContext);
+    const [formObj, setFormObj] = useState({
+    user_name:"",
+    user_email:"",
+    message:"",
+    })
 
-  const form = useRef();
-  const sendEmail = (e) => {
+    const [troll, setTroll] = useState(true)
+    // let troll = true
+
+    const [trollClass, setTrollClass] = useState("standard")
+
+    const toyPpl = ()=>{
+      trollClass === "standar" || trollClass === "left"? setTrollClass("right"): setTrollClass("left")
+    }
+
+
+    useEffect(()=>{
+       if(formObj.user_name && formObj.user_email && formObj.message) setTroll(false)
+      else{
+        setTroll(true)
+      }
+    },[formObj])
+
+    const handleInput = (e)=>{
+      setFormObj({...formObj,[e.target.name]:e.target.value})
+    }
+
+    const form = useRef();
+    const sendEmail = (e) => {
     e.preventDefault();
 
     emailjs.sendForm(VITE_SERVICE_ID, VITE_TEMPLATE_ID, form.current, VITE_PUBLIC_KEY)
-      .then((result) => {
-          console.log(result.text);
-          form.current.childNodes.forEach(e=>{
+        .then((result) => {
+            console.log(result.text);
+            form.current.childNodes.forEach(e=>{
             e.value = ""
-          })
-      }, (error) => {
+            })
+        }, (error) => {
           console.log(error.text);
-      });
+        });
       
-  };
+    };
 
   return (
     <form ref={form} className="form" onSubmit={sendEmail}>
       
-      <input placeholder='Nombre*' className={`input ${theme}`} type="text" name="user_name" id='"user_name"' />
+      <input placeholder='Nombre*' className={`input ${theme}`} type="text" value={formObj.user_name} onChange={handleInput} name="user_name" id='"user_name"' />
       
-      <input placeholder='Email' className={`input ${theme}`} type="email" name="user_email" />
+      <input placeholder='Email' className={`input ${theme}`} type="email" value={formObj.user_email} onChange={handleInput} name="user_email" />
       
-      <textarea className={`${theme}`} name="message" />
-      <button className={`submit ${theme}`} type="submit">Enviar</button>
+      <textarea className={`${theme}`} name="message" value={formObj.message} onChange={handleInput} />
+      <button  onMouseOver={toyPpl}
+        className={troll? trollClass:`standard ${theme}`} type="submit">Enviar
+      </button>
     </form>
   );
 };
